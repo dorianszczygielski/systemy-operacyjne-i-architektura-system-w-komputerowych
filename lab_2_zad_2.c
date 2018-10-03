8944dc5ab1d81f01f11123a9e4894111177791ff
dorianszczygielski <43792203+dorianszczygielski@users.noreply.github.com>
2018-10-03 15:05:51 +0200

    Update lab_2_zad_2.c

--- lab_2_zad_2.c
+++ lab_2_zad_2.c
@@ -1,1 +1,27 @@
+/*
 
+	© Dorian Szczygielski / Zachodniopomorski Uniwersytet Technologiczny w Szczecinie / Wydział Elektryczny
+	Systemy operacyjne i architektura systemów komputerowych
+	lab_2 / zadanie_2
+
+*/
+
+#include <stdio.h>
+#include <signal.h>
+void catch_SIGINT(int signal_number);
+
+int main (void) {
+
+   signal(SIGINT, catch_SIGINT);
+
+   printf("Wysylam sygnal...\n");
+   raise(SIGINT);
+
+   printf("Wychodze...\n");
+
+   return(0);
+}
+
+void catch_SIGINT(int signal_number) {
+   printf("Przechwycilem sygnal!\n");
+}
