1f9345067f7494fa569a98e7d74b8d66490a1972
dorianszczygielski <43792203+dorianszczygielski@users.noreply.github.com>
2018-10-10 22:24:30 +0200

    Update lab_3_zad_1_slave.c

--- lab_3_zad_1_slave.c
+++ lab_3_zad_1_slave.c
@@ -5,7 +5,7 @@
 // structure for message queue
 struct mesg_buffer {
     long mesg_type;
-    char mesg_text[100];
+    char mesg_text[255];
 } message;
 
 int main()
@@ -13,21 +13,25 @@ int main()
     key_t key;
     int msgid;
 
+    //convert a pathname and a project identifier to a System V IPC
     // ftok to generate unique key
-    key = ftok("progfile", 65);
-
+    key = ftok("uniquename", 66);
+    
     // msgget creates a message queue
     // and returns identifier
+    //CHMOD
     msgid = msgget(key, 0666 | IPC_CREAT);
 
     // msgrcv to receive message
+    //1 = msgtyp
+    //0 = msgflg
     msgrcv(msgid, &message, sizeof(message), 1, 0);
 
     // display the message
-    printf("Data Received is: %s \n",
-                    message.mesg_text);
+    printf("Data Received is: %s \n", message.mesg_text);
 
     // to destroy the message queue
+    // IPC_RMID - Mark the segment to be destroyed
     msgctl(msgid, IPC_RMID, NULL);
 
     return 0;
