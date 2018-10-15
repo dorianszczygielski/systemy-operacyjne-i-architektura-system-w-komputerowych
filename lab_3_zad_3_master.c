16f4a6278bb6645546fe7ef62e27077eabf8da18
dorianszczygielski <43792203+dorianszczygielski@users.noreply.github.com>
2018-10-15 09:34:51 +0200

    Update lab_3_zad_3_master.c

--- lab_3_zad_3_master.c
+++ lab_3_zad_3_master.c
@@ -27,3 +27,43 @@ int main(int argc, char** argv) {
 
   return 0;
 }
+
+struct myMessage {
+long type;
+int a;
+} msgSnd, msgRcv1, msgRcv2;
+
+int main() {
+
+key_t key;
+int msgid;
+int pid;
+
+printf("Write your number: ");
+scanf("%d", &msgSnd.a);
+
+printf("Slave first pid: ");
+scanf("%d", &pid);
+
+
+msgSnd.type = pid;
+
+key = ftok("./", 20);
+msgid = msgget(key, 0666 | IPC_CREAT);
+msgsnd(msgid, &msgSnd, sizeof(msgSnd), 0);
+
+printf("Slave second pid: ");
+scanf("%d", &pid);
+msgSnd.type = pid;
+msgsnd(msgid, &msgSnd, sizeof(msgSnd), 0);
+
+msgrcv(msgid, &msgRcv1, sizeof(msgRcv1), 1, 1);
+printf("\n\n First number: %d\n\n", msgRcv1.a);
+
+msgrcv(msgid, &msgRcv2, sizeof(msgRcv2), 2, 2);
+printf("\n\n First number: %d\n\n", msgRcv2.a);
+
+msgctl(msgid, IPC_RMID, NULL);
+
+return 0;
+}
