f5962838b1acb81e74a5b98d41c2a6ebbe861eb4
dorianszczygielski <43792203+dorianszczygielski@users.noreply.github.com>
2018-11-13 21:35:32 +0100

    Create lab_7_zad_3_slave.c

---
+++ lab_7_zad_3_slave.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/shm.h>
+ 
+int main()
+{
+    int input;
+    bool me = false;
+    char message[100];
+    int semafor = semget(ftok(".", 'a'), 1, IPC_CREAT | 0666);
+    struct sembuf lock = { 0, -1, 0 };
+    struct sembuf unlock = { 0, 1, 0 };
+   
+    int shmid = shmget(ftok(".", 'A'), 1024, IPC_CREAT | 0666);
+    char* shmpointer = shmat(shmid, NULL, SHM_RDONLY);
+   
+    semctl(semafor, 0, SETVAL, 1);
+   
+    while(true)
+    {
+        printf("\n\nInput: ");
+        scanf("%d", &input);
+       
+        if(input == 1)
+        {
+            semop(semafor, &lock, 1);
+            printf("\nSemaphore Locked\n\n");
+            printf("\nMessage Received: %s\n\n", shmpointer);
+            semop(semafor, &unlock, 1);
+            me = true;
+        }
+        /*
+        if(input == 2 && me == true)
+        {
+            semop(semafor, &unlock, 1);
+            printf("\nSemaphore Unlocked\n\n");
+            me = false;
+        }
+        */
+    }
+   
+    shmdt(&shmpointer);
+   
+    semctl(semafor, 0, IPC_RMID);
+   
+    return 0;
+}
